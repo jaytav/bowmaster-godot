3498e363cc5f94ab77865ddc38d42ff38b500284
Jayden <jayden.tavares@hotmail.com>
2020-08-06 21:20:03 +1000

    Game Pause using ESC Update

--- Scenes/Game/GameGUI.gd
+++ Scenes/Game/GameGUI.gd
@@ -6,4 +6,4 @@ onready var _game_menu = $GameMenu
 func _input(event):
 	if event.is_action_pressed("pause"):
 		_game_menu.visible = !_game_menu.visible
-		get_tree().pause(_game_menu.visible)
+		get_tree().paused = _game_menu.visible
