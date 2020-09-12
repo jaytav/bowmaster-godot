0b8a70ef181f57e325c9bef3ea9e0285484c3e85
Jayden <jayden.tavares@hotmail.com>
2020-09-12 17:12:01 +1000

    Update Playe Health UI to HealthBar

--- src/scenes/game/GameGUI.gd
+++ src/scenes/game/GameGUI.gd
@@ -6,7 +6,9 @@ onready var _game_menu = $GameMenu
 onready var _player_stats: Node = MainWorld.get("Game/Player/Stats")
 
 # Health
-onready var _player_health_ui: Label = $PlayerHealth
+onready var _player_health_over_ui: TextureProgress = $PlayerHealth/Over
+onready var _player_health_under_ui: TextureProgress = $PlayerHealth/Under
+onready var _player_health_update_tween: TextureProgress = $PlayerHealth/UpdateTween
 onready var _player_health: Health = _player_stats.get_node("Health")
 
 # Ammo
@@ -31,7 +33,9 @@ func _input(event: InputEvent) -> void:
 
 
 func _on_Player_health_updated(value: int) -> void:
-	_player_health_ui.text = str(value) + " / " + str(_player_health.max_value)
+	_player_health_over_ui.value = (float(value) / _player_health.max_value) * 100
+	_player_health_update_tween.interpolate_property(_player_health_under_ui, "value", _player_health_under_ui.value, _player_health_over_ui.value, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	_player_health_update_tween.start()
 
 
 func _on_Player_ammo_updated(value: int) -> void:
